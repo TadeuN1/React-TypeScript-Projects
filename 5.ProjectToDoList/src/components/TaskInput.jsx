65733ef7d1d638a5445e2727556faab65aea22f8
TadeuN1 <tadeunrs@gmail.com>
2025-02-22 11:41:06 -0300

    todolist project

---
+++ 5.ProjectToDoList/src/components/TaskInput.jsx
@@ -0,0 +1,27 @@
+import React, { use } from 'react'
+import { useState } from 'react'
+
+const TaskInput = ({onAddTask}) => {
+    const [input, setInput] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if(input.trim()){
+            onAddTask(input);
+            setInput("")
+        }
+    } 
+
+  return (
+    <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Adicione uma nova tarefa'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        />
+        <button type='submit'> Adicionar </button>
+    </form>
+  )
+}
+
+export default TaskInput
