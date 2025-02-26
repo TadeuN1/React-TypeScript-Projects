24e0ba68f955e8b62b0f830a016b8e9781c34f43
TadeuN1 <tadeunrs@gmail.com>
2025-02-26 10:39:54 -0300

    end project album

--- 9.ProjectAlbum/src/components/Searchbar.jsx
+++ 9.ProjectAlbum/src/components/Searchbar.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
-const Searchbar = () => {
+const Searchbar = ({setQuery, setCategoria, setActivateSearch}) => {
+
+  const categorias = [
+    "Natureza",
+    "Pessoas",
+    "Tecnologia",
+    "Animais",
+    "Esportes"
+  ]
+
   return (
-    <div>Searchbar</div>
+    <div className='search-bar'>
+      <input type='text' placeholder='Pesquisar fotos...' onChange={(e) => setQuery(e.target.value)}/>
+      <button onClick={() => setActivateSearch(true)}> Pesquisar </button>
+      <select onChange={(e) => {
+        setCategoria(e.target.value);
+        setActivateSearch(true)
+      }}> {categorias.map((categoria) => (
+        <option key={categoria} value={categoria}> {categoria} </option>
+      ))} </select>
+    </div>
   )
 }
 
